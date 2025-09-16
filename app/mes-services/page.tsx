663b5c0585d3d09d176c99e4022cc155b73c6f29
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-09-16 10:57:54 +0200

    feat: ajouter la structure de la page "Mes Services" avec un conteneur et une section

--- app/mes-services/page.tsx
+++ app/mes-services/page.tsx
@@ -1,5 +1,14 @@
+import PageContainer from "@/components/page-container";
+import Section from "@/components/section";
+
 import React from "react";
 
 export default function MesServices() {
-  return <div>Mes Services</div>;
+  return (
+    <PageContainer>
+      <Section title="Une approche stratégique complète" bgColor="bg-slate-900">
+        TEst
+      </Section>
+    </PageContainer>
+  );
 }
