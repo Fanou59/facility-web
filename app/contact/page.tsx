c2ba493c7afce3960f297b6d039e7b1c8e3d660a
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-09-17 10:27:57 +0200

    feat: ajouter un script pour intégrer le widget de planification de réunion Zcal

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,19 @@
+"use client";
 import { ContactForm } from "@/components/contactForm";
 import PageContainer from "@/components/page-container";
 import Section from "@/components/section";
+import { useEffect } from "react";
 
 export default function Contact() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://static.zcal.co/embed/v1/embed.js";
+    script.async = true;
+    document.body.appendChild(script);
+    return () => {
+      document.body.removeChild(script);
+    };
+  });
   return (
     <PageContainer>
       <Section
@@ -14,6 +25,12 @@ export default function Contact() {
       ></Section>
       <Section title="Discutons de votre projet" bgColor="bg-slate-100">
         <ContactForm />
+
+        <div className="zcal-inline-widget mt-8">
+          <a href="https://zcal.co/i/tDx2iy-O">
+            30 Minute Meeting - Schedule a meeting
+          </a>
+        </div>
       </Section>
     </PageContainer>
   );
