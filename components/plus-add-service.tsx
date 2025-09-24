bc0b5fda71adef71d4ee8b8b37b5f37235161c24
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-09-24 10:48:09 +0200

    feat: améliorer l'affichage du composant PlusAddService en ajoutant un texte descriptif

--- components/plus-add-service.tsx
+++ components/plus-add-service.tsx
@@ -2,11 +2,8 @@ import { PlusCircle } from "lucide-react";
 
 export default function PlusAddService() {
   return (
-    <div className="flex justify-center">
-      <span className="flex items-center gap-2">
-        <PlusCircle className="fill-orange-500 text-white" size={40} />
-        Ajouter un service
-      </span>
+    <div className="flex justify-center items-center h-full">
+      <PlusCircle className="fill-orange-500 text-white" size={40} />
     </div>
   );
 }
