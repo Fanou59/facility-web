00bfd5701c853a2d6ba772d049d91fab11a23db4
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-10-10 12:50:00 +0200

    feat: refactor la gestion de l'instance Prisma pour améliorer la logique de création

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,6 +4,19 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+let prismaInstance: PrismaClient;
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prismaInstance = new PrismaClient({
+    log: ["error"],
+  });
+} else {
+  if (!globalForPrisma.prisma) {
+    globalForPrisma.prisma = new PrismaClient({
+      log: ["query", "error", "warn"],
+    });
+  }
+  prismaInstance = globalForPrisma.prisma;
+}
+
+export const prisma = prismaInstance;
