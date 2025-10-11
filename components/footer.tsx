3736862c03933f6cc8a51785af2e2e8397b830dd
Ste[PH]ane <steph.guery@gmail.com>
2025-10-11 09:40:56 +0200

    feat: ajouter la page des mentions légales avec le contenu légal requis

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import PageContainer from "./page-container";
 
 export default function Footer() {
@@ -5,6 +6,7 @@ export default function Footer() {
     <PageContainer>
       <footer className="bg-gray-800 text-gray-300 py-6 ">
         <div className="container mx-auto px-6 text-center">
+          <Link href="/mentions-legales">Mentions légales</Link>
           <p>&copy; 2025 Facility-web. Tous droits réservés.</p>
         </div>
       </footer>
