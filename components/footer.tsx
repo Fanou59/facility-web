0509aaca3084fabee14f347245d1a286673b0e60
Ste[PH]ane <steph.guery@gmail.com>
2025-10-11 10:47:07 +0200

    feat: ajouter des séparateurs dans le pied de page entre les liens légaux

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,9 +8,12 @@ export default function Footer() {
         <div className="container mx-auto px-6 text-center">
           <div className="flex gap-2 justify-center">
             <Link href="/mentions-legales">Mentions légales</Link>
+            <span className="text-gray-500">|</span>
             <Link href="/politique-confidentialite">
               Politique de confidentialité
             </Link>
+            <span className="text-gray-500">|</span>
+
             <Link href="/politique-cookie">Politique de cookies</Link>
           </div>
           <p>&copy; 2025 Facility-web. Tous droits réservés.</p>
