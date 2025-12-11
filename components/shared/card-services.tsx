1c98baae467f1d7828e3241bbfd9b3bf7d1a76f1
Stephane Guery <58395456+Fanou59@users.noreply.github.com>
2025-12-11 15:50:16 +0100

    feat: remplacer le gestionnaire de clic par un lien pour naviguer vers les services

--- components/shared/card-services.tsx
+++ components/shared/card-services.tsx
@@ -1,6 +1,5 @@
-"use client";
 import Image from "next/image";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
 
 type CardServicesProps = {
@@ -16,30 +15,28 @@ export default function CardServices({
   imageUrl,
   alt,
 }: CardServicesProps) {
-  const router = useRouter();
-  const handleClick = () => {
-    router.push("/mes-services");
-  };
   return (
-    <Card
-      className="shadow-lg transform hover:scale-105 transition-transform duration-300 cursor-pointer relative"
-      onClick={handleClick}
+    <Link
+      href="/mes-services"
+      className="block shadow-lg transform hover:scale-105 transition-transform duration-300 cursor-pointer relative"
     >
-      <CardHeader className="flex items-center justify-center gap-2">
-        <Image height={50} width={50} src={imageUrl} alt={alt} />
-        <CardTitle>{title}</CardTitle>
-      </CardHeader>
-      <CardContent className="pb-10 md:pb-0">
-        {resume && (
-          <p
-            className="text-gray-600 text-sm"
-            dangerouslySetInnerHTML={{ __html: resume }}
-          />
-        )}
-      </CardContent>
-      <button className="absolute bottom-2 right-2 w-6 h-6 bg-orange-500 hover:bg-orange-600 text-white rounded-full flex items-center justify-center text-sm font-bold transition-colors md:hidden">
-        +
-      </button>
-    </Card>
+      <Card className="h-full">
+        <CardHeader className="flex items-center justify-center gap-2">
+          <Image height={50} width={50} src={imageUrl} alt={alt} />
+          <CardTitle>{title}</CardTitle>
+        </CardHeader>
+        <CardContent className="pb-10 md:pb-0">
+          {resume && (
+            <div
+              className="text-gray-600 text-sm"
+              dangerouslySetInnerHTML={{ __html: resume }}
+            />
+          )}
+        </CardContent>
+        <div className="absolute bottom-2 right-2 w-6 h-6 bg-orange-500 hover:bg-orange-600 text-white rounded-full flex items-center justify-center text-sm font-bold transition-colors md:hidden">
+          +
+        </div>
+      </Card>
+    </Link>
   );
 }
